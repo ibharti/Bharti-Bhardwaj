9cf3395ab87333ba24fa91489516cc19f95ba320
ibharti <bhartibhardwaj1207@gmail.com>
2022-01-24 20:32:19 +0100

    commit

--- bb_port/main.js
+++ bb_port/main.js
@@ -3,7 +3,7 @@ const hamburger = document.querySelector('.menu-btn__burger');
 
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
-const navItems = document.querySelector('.menu-nav__item');
+const navItem = document.querySelector('.menu-nav__item');
 
 let showMenu = false;
 
@@ -15,18 +15,21 @@ function toggleMenu() {
 
         nav.classList.add('open');
         menuNav.classList.add('open');
-        navItems.forEach(item => item.classList.add('open'));
+        navItem.forEach(item => item.classList.add('open'));
 
         showMenu = true;
     }
     else {
+
         hamburger.classList.remove('open');
+
 
         nav.classList.remove('open');
         menuNav.classList.remove('open');
-        navItems.forEach(item => item.classList.remove('open'));
 
+        navItem.forEach(item => item.classList.remove('open'));
         showMenu = false;
+
     }
 
 }
